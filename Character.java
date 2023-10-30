7e7732a030f41fcad9efee8239fcf85b715e3f83
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-30 09:54:09 +0100

    task1

---
+++ Character.java
@@ -0,0 +1,30 @@
+public abstract class Character {
+
+    protected String name;
+    protected String RPGClass;
+    protected int life;
+    protected int agility;
+    protected int strength;
+    public int wit;
+
+    protected Character(String name, String RPGClass) {
+        this.name = name;
+        this.RPGClass = RPGClass;
+        this.life = 50;
+        this.agility = 2;
+        this.strength = 2;
+        this.wit = 2;
+    }
+
+    public String getName() {return name; }
+    public String getRPGClass() { return RPGClass; }
+    public int getLife() {return life ; }
+    public int getAgility() {
+        return agility;
+    }
+    public int getStrength() {return strength; }
+    public int getWit() {return wit;}
+    public void attack(String argument) {
+        System.out.println(name + ": Rrrrrrrrr....");
+    }
+}
