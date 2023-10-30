ddafa04e2b331a818ccb852549cf3772519233c5
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-30 10:34:00 +0100

    task3

--- Character.java
+++ Character.java
@@ -1,4 +1,4 @@
-public abstract class Character {
+public abstract class Character implements Movable {
 
     protected String name;
     protected String RPGClass;
@@ -26,5 +26,24 @@ public abstract class Character {
     public int getWit() {return wit;}
     public void attack(String argument) {
         System.out.println(name + ": Rrrrrrrrr....");
+    }
+
+    @Override
+    public void moveRight() {
+        System.out.println(getName() + ": moves right");
+    }
+
+    @Override
+    public void moveLeft() {
+        System.out.println(getName() + ": moves left");
+    }
+    @Override
+    public void moveForward() {
+        System.out.println(getName() + ": moves forward");
+    }
+
+    @Override
+    public void moveBack() {
+        System.out.println(getName() + ": moves back");
     }
 }
